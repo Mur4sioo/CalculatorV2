c311e540484e7db30a1a21aa7c3f5c97ea77a41e
Kacper <kacpermurawski2000@gmail.com>
2024-02-22 19:48:09 +0100

    22.02.2024

--- CalculatorWPF/App.xaml.cs
+++ CalculatorWPF/App.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace CalculatorWPF
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CalculatorWPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
-    {
-    }
 }
